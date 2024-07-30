9b82ce013e8f119f2973894963caa4b33266da57
Nitin gour <gournitin39@gmail.com>
2024-07-30 08:41:05 +0530

    dynamic routes | useParams | useLocation | wildcard route | outlet

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Create from "./components/Create";
 import Show from "./components/Show";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import Detail from "./components/Detail";
 import TodoContext from "./context/TodoContext";
 
 const App = () => {
@@ -13,7 +14,19 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/create" element={<Create />} />
-                <Route path="/show" element={<Show />} />
+              
+                <Route path="/show" element={<Show />}>
+                    <Route path="/show/:id" element={<Detail />} />
+                </Route>
+
+                <Route
+                    path="*"
+                    element={
+                        <h1 className="text-center mt-10 text-3xl text-red-400">
+                            Page Not Found
+                        </h1>
+                    }
+                />
             </Routes>
         </div>
     );
